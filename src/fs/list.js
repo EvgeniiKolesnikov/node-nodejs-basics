6c5d0ae29d0d19fee9466c18967ed09cc21a066f
Evgenii Kolesnikov <jekow@yandex.ru>
2022-06-05 20:02:43 +0300

    feat: implement fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import { readdir } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+// +6 list.js - implement function that prints all array of filenames from 'files' folder into console (if files folder doesn't exists Error with message 'FS operation failed' must be thrown)
+
 export const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const targetFolder = 'files';
+  const targetPath = join(__dirname, targetFolder);
+
+  await readdir(targetPath, { flag: 'wx' })
+    .then((data) => console.log(data.toString().split(',')))
+    .catch((err) => {
+      err.message = 'FS operation failed';
+      console.error(err);
+    });
 };
+
+list();
