ef5a388cce8ed37ad1953accb38b4a84b3be5495
Evgenii Kolesnikov <jekow@yandex.ru>
2022-06-05 20:02:16 +0300

    feat: implement fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import { writeFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+// +6 create.js - implement function that creates new file fresh.txt with content 'I am fresh and young' inside of the files folder (if file already exists Error with message 'FS operation failed' must be thrown)
+
 export const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const targetFolder = 'files';
+  const targetFile = 'fresh.txt';
+  const targetText = 'I am fresh and young';
+  const targetPath = join(__dirname, targetFolder, targetFile);
+
+  await writeFile(targetPath, targetText, { flag: 'wx' }).catch((err) => {
+    err.message = 'FS operation failed';
+    console.error(err);
+  });
 };
+
+create();
