47786369af8016a800b21279680c27efccbe616d
Evgenii Kolesnikov <jekow@yandex.ru>
2022-06-05 20:02:51 +0300

    feat: implement fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,23 @@
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+// +6 read.js - implement function that prints content of the 'fileToRead.txt' into console (if there's no file fileToRead.txt Error with message 'FS operation failed' must be thrown)
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const targetFolder = 'files';
+  const targetFile = 'fileToRead.txt';
+  const targetPath = join(__dirname, targetFolder, targetFile);
+
+  await readFile(targetPath)
+    .then((data) => console.log(data.toString().replace(/\s+/g, ' ')))
+    .catch((err) => {
+      err.message = 'FS operation failed';
+      console.error(err);
+    });
 };
+
+read();
