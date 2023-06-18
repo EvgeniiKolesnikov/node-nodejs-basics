145ee5ac903ae29a76d05e7355152a524e50087e
Evgenii Kolesnikov <jekow@yandex.ru>
2023-06-19 02:43:18 +0300

    feat: implement streams - read, write, transform

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+
+// read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const rs = createReadStream(`${__dirname}/files/fileToRead.txt`, 'utf-8');
+
+  await pipeline(rs, stdout);
 };
+
+read();
