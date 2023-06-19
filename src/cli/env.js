fff9bfd9b236201d08d9fc0f323d63944c0242bc
Evgenii Kolesnikov <jekow@yandex.ru>
2023-06-19 03:11:15 +0300

    feat: implement cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+// env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+
 export const parseEnv = () => {
-    // Write your code here 
+  const output = Object.entries(process.env)
+    .filter(([key, value]) => key.startsWith('RSS_'))
+    .map((prop) => prop.join('='))
+    .join('; ');
+  console.log(output);
 };
+
+parseEnv();
