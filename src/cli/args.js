4508f18c658dfda71a842e6a3d82a0b66aad2e16
Evgenii Kolesnikov <jekow@yandex.ru>
2023-06-19 04:12:47 +0300

    feat: implement cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
+// args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
+
 export const parseArgs = () => {
-    // Write your code here 
+  const output = process.argv
+    .map((str, i, arr) =>
+      str.startsWith('--') ? `${str.slice(2)} is ${arr[i + 1]}` : ''
+    )
+    .filter((str) => str)
+    .join(', ');
+  console.log(output);
 };
+
+parseArgs();
