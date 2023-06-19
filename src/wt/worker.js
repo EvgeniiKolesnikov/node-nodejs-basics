0e8ce1b4f4b39786a85fe5282cee8b1ca626f99a
Evgenii Kolesnikov <jekow@yandex.ru>
2023-06-20 02:01:28 +0300

    feat: implement wt - main, worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,13 @@
+import { parentPort, workerData } from 'node:worker_threads';
+
+// worker.js - extend given function to work with data received from main thread and implement function which sends result of the computation to the main thread
+
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(workerData));
 };
+
+sendResult();
